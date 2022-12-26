385d48ef38afaa206ae616b0b2e22fe8d2bebcda
oscarsmbaquero <94797307+oscarsmbaquero@users.noreply.github.com>
2022-12-26 13:24:06 +0100

    form reservas

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,4 +1,4 @@
-import  React,{useEffect} from "react";
+import React, { useEffect } from "react";
 import Button from "@mui/material/Button";
 import CssBaseline from "@mui/material/CssBaseline";
 import TextField from "@mui/material/TextField";
@@ -8,15 +8,17 @@ import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { useNavigate } from "react-router-dom";
- import Swal from "sweetalert2";
-import emailjs from '@emailjs/browser';
+import Swal from "sweetalert2";
+import emailjs from "@emailjs/browser";
 import { IntlProvider, FormattedMessage } from "react-intl";
 import "./Contact.scss";
 import Aos from "aos";
 import "aos/dist/aos.css";
-
-
-
+import { MobileDatePicker } from "@mui/x-date-pickers/MobileDatePicker";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import Select from "@mui/material/Select";
+//import  TextArea  from '@material-ui/core';
 
 // function Copyright(props) {
 //   return (
@@ -39,28 +41,43 @@ const theme = createTheme();
 
 export default function SignUp() {
   let navigate = useNavigate();
+  const [comensales, setComensales] = React.useState("");
+
+  const handleChange = (event) => {
+    if (event.target.value === 10) {
+      Swal.fire({
+        //title: "Success!",
+        text: "Para Grupos superiores a 10 personas consulte disponibiliad por teléfono",
+        icon: "error",
+        confirmButtonText: "Ok",
+      });
+    } else {
+      setComensales(event.target.value);
+    }
+  };
+  console.log(comensales, "comensales");
 
   const sendMail = (e) => {
     e.preventDefault();
-    try {
-      emailjs
-        .sendForm(
-          "service_kqpmbaa",
-          "template_2c1n23x",
-          e.target,
-          "dso8n6rVU1ADlfbV4"
-        )
-        .then((response) => console.log(response));
-      Swal.fire({
-        title: "Success!",
-        text: " Mensaje enviado. Gracias!!!!!",
-        icon: "success",
-        confirmButtonText: "Ok",
-      });
-      navigate("/");
-    } catch (error) {
-      navigate("/");
-    }
+    // try {
+    //   emailjs
+    //     .sendForm(
+    //       "service_kqpmbaa",
+    //       "template_2c1n23x",
+    //       e.target,
+    //       "dso8n6rVU1ADlfbV4"
+    //     )
+    //     .then((response) => console.log(response));
+    //   Swal.fire({
+    //     title: "Success!",
+    //     text: " Mensaje enviado. Gracias!!!!!",
+    //     icon: "success",
+    //     confirmButtonText: "Ok",
+    //   });
+    //   navigate("/");
+    // } catch (error) {
+    //   navigate("/");
+    // }
   };
 
   useEffect(() => {
@@ -69,71 +86,116 @@ export default function SignUp() {
 
   return (
     <div className="form" data-aos="fade-left">
-    <ThemeProvider theme={theme}>
-      <Container component="main" maxWidth="md">
-        <CssBaseline />
-        <Box
-          sx={{
-            //marginTop: 8,
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-          }}
-        >
+      <ThemeProvider theme={theme}>
+        <Container component="main" maxWidth="md">
+          <CssBaseline />
           <Box
-            component="form"
-            onSubmit={sendMail}
-            noValidate
-            sx={{ mt: 5 }}
+            sx={{
+              //marginTop: 8,
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+            }}
           >
-         <Typography>
-          <p><FormattedMessage id="app.form"/></p>
-         </Typography> 
-            <TextField 
-              margin="normal"
-              required
-              fullWidth
-              name="mail"
-              label=<FormattedMessage id="app.mail"/>
-              type="mail"
-              id="mail"
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="name"
-              label=<FormattedMessage id="app.name"/>
-              type="text"
-              id="name"
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="question"
-              label=<FormattedMessage id="app.comments"/>
-              type="text"
-              id="password"
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              
-              sx={{
-                 mt: 2,
-                 mb: 0,
-                 borderRadius:8,
-                 color:'white' }}
-            >
-              <FormattedMessage id="app.send"/>
-            </Button>
-            {/* <Copyright sx={{ mt: 8, mb: 4 }} /> */}
+            <Box component="form" onSubmit={sendMail} noValidate sx={{ mt: 5 }}>
+              <Typography>
+                <p>
+                  <FormattedMessage id="app.form" />
+                </p>
+              </Typography>
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="mail"
+                label=<FormattedMessage id="app.mail" />
+                type="mail"
+                id="mail"
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="name"
+                label=<FormattedMessage id="app.name" />
+                type="text"
+                id="name"
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="question"
+                label=<FormattedMessage id="app.tlf" />
+                type="text"
+                id="password"
+              />
+              <TextField
+                id="outlined-multiline-flexible"
+                margin="normal"
+                label=<FormattedMessage id="app.comments" />
+                type="text"
+                rowsMax="4"
+                multiline
+                fullWidth
+                required
+                variant="outlined"
+              />
+              <TextField
+                id="datetime-local"
+                required
+                label=<FormattedMessage id="app.fecha" />
+                type="datetime-local"
+                margin="normal"
+                defaultValue="2022-05-24T10:30"
+                fullWidth
+                InputLabelProps={{
+                  shrink: true,
+                }}
+              />
+              <InputLabel id="demo-simple-select-label">
+                <FormattedMessage id="app.comensales" />
+              </InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={comensales}
+                required
+                margin="normal"
+                fullWidth
+                label="Age"
+                onChange={handleChange}
+              >
+                <MenuItem value={1}>1</MenuItem>
+                <MenuItem value={2}>2</MenuItem>
+                <MenuItem value={3}>3</MenuItem>
+                <MenuItem value={4}>4</MenuItem>
+                <MenuItem value={5}>5</MenuItem>
+                <MenuItem value={6}>6</MenuItem>
+                <MenuItem value={7}>7</MenuItem>
+                <MenuItem value={8}>8</MenuItem>
+                <MenuItem value={9}>9</MenuItem>
+                <MenuItem value={10}>+ 10</MenuItem>
+              </Select>
+
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                sx={{
+                  mt: 2,
+                  mb: 0,
+                  borderRadius: 8,
+                  color: "white",
+                }}
+              >
+                <FormattedMessage id="app.send" />
+              </Button>
+              {/* <Copyright sx={{ mt: 8, mb: 4 }} /> */}
+            </Box>
           </Box>
-        </Box>
-      </Container>
-    </ThemeProvider>
+        </Container>
+      </ThemeProvider>
     </div>
   );
 }
